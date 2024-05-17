cf2df623751cb28ae8d1549d6fc45353fef2fb65
victormj7 <victor.moratojesus@gmail.com>
2024-05-17 08:30:42 -0300

    retomada2e

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from "react"
+import './App.css'
 
-const App = () =>(
-  <>
+const App = () => (
+  <div className="App">
+    <section>
+    <header>
   AULA INICIAL DE REACTJS
-  </>
+    </header>
+  <content>
+Vamos codar em Reactjs
+  </content>
+    <footer>
+   @itb2024
+    </footer>
+    </section>
+    </div>
+
 )
 
-export default App
+export default App;
